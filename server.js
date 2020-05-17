1a2602ae9e1c5bb4050a860d7a182f1aa8014429
Tpoppp <olson0002@yahoo.com>
2020-05-16 23:49:09 -0700

    changed port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 //const apiRoutes = require("./routes/api-routes");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
